43501aa499e81c28f4a5250e58b4005000e8c7d4
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2023-09-28 16:55:28 +0300

    Добавление Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -6,14 +6,28 @@
 // 12-> 2
 // 85 -> 8
 
-int number = new Random().Next(10, 100); // 99+1
-Console.WriteLine($"Случайное число из отрезка [10, 99] -> {number}");
+int MaxDigit (int num)
+{
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
 
-int firstDigit = number / 10; //78 / 10 = 7
-int secontDigit = number % 10; //78 = 70 + 8
 
-if(firstDigit > secontDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");// второй способ
-else Console.WriteLine ($"Наибольшая цифра числа -> {secontDigit}");
+int number = new Random().Next(10, 100); // 99 + 1
+Console.WriteLine($"Cлучайное число из отрезка [10, 99] -> {number}");
 
-int maxDigit = firstDigit > secontDigit ? firstDigit : secontDigit; // второй способ Тернарный оператор, где ? это тогда, а : иначе
-Console.WriteLine ($"Наибольшая цифра числа -> {maxDigit}");
+// int firstDigit = number / 10; // 78 / 10 = 7
+// int secondDigit = number % 10; // 78 = 70 + 8
+
+// if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; // Тернарный оператор
+// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int maxDigit1 = MaxDigit(54);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit1}"); // это пример
