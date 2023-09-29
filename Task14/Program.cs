7bfe1f05fbb2ae41864aabcf9cb5f875ae60d6c7
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2023-09-29 12:22:13 +0300

    Добавление Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+int GetUserInput(string str)
+{
+    Console.Write($"{str}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+bool DivisionTo7and23(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+int number = GetUserInput("Enter a number: ");
+bool result = DivisionTo7and23(number);
+
+Console.Write(result ? $"{number} -> да" : $"{number} -> нет");
